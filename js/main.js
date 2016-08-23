8cb0a9d1d2257492e4b1dd03d58bdd28ff073754
samyounger <sam_younger@hotmail.com>
2016-08-23 21:07:36 +0100

    deleted all the styling and some of the HTML, and started again to clean up the code. Also deleted the jquery code. Created a new HTML page for projects, and added a description and list of the first two weeks of GA homework

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('h2').click(function() {
-		if ($(this).next('p').length > 0) {
-		$(this).next().slideToggle('show');
-		} else {
-		$(this).next().next().slideToggle('show');
-		};
-	});
-});
